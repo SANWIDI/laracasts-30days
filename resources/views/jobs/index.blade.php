2f7680cf36ce72b06b49dfe819155784aa7015f3
SANWIDI <112139535+SANWIDI@users.noreply.github.com>
2025-05-02 19:12:44 -0500

    video 26 complete

--- resources/views/jobs/index.blade.php
+++ resources/views/jobs/index.blade.php
@@ -12,7 +12,8 @@
                     {{ $job->employer->name }}
                 </div>
                     <div>
-                    <strong>{{ $job['title'] }}:</strong> Pays {{ $job['salary'] }} per year.
+{{--                    <strong>{{ $job['title'] }}:</strong> Pays {{ $job['salary'] }} per year.--}}
+                        <strong class="text-laracasts">{{ $job['title'] }}:</strong> Pays {{ $job['salary'] }} per year.
 
                 </div>
                 </a>
